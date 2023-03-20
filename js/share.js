f963a6a46d346ac3efca3cc86a25a57611d12f0d
dianait <dianahdezsoler@gmail.com>
2023-03-20 12:33:01 +0100

    Update twitter share text

--- js/share.js
+++ js/share.js
@@ -1,5 +1,5 @@
 export function getUrlShareTwitter(question) {
     const BASE_URL = "https://twitter.com/intent/tweet";
-    const content = `?text=⭐️ New question ⭐️ 👉  ${question} Get another one at &url=https://ranther.vercel.app`;
+    const content = `?text=⭐️ New question ⭐️ 👉  ${question} 💪 @dianait_ Get another one at &url=https://ranther.vercel.app`;
     return BASE_URL + content;
   } 
