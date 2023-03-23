9fcaeff955b225c5a92bd17200e5802b16d2ebb8
Diana Hernandez <diana.hernandez@xing.com>
2023-03-23 10:05:11 +0100

    Fix twitter shared

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ import { showSpinner, hideSpinner } from "./loading.js";
 import { 
 
   generateButton, 
-  openIAOutput, 
   shareTwitter, 
   option1Button,
   option2Button,
@@ -16,27 +15,15 @@ import {
 
 (async () =>{
   generateButton.onclick = await getQuestion;
-  const exampleQuestion = "Would you rather be a cat 🐱 or a dog 🐶?"
-  question.innerText = exampleQuestion
-  const answers = splitAnswer(exampleQuestion)
-  option1.innerText = answers.option1
-  option2.innerText = answers.option2
-  option1Button.setAttribute("value", answers.option1)
-  option2Button.setAttribute("value", answers.option2)
-  const answerSelected = document.querySelector('input[name=option]:checked').value;
-  shareTwitter.setAttribute("href", getUrlShareTwitter(exampleQuestion, answerSelected));
+  setup("Would you rather be a cat 🐱 or a dog 🐶?")
 })();
 
 
 async function  getQuestion() {
   showSpinner()
     let openIAResult = await openIARequest();
-    const question = openIAResult.trim();
-    openIAOutput.innerText = question;
-    const answers = splitAnswer(question)
-    option1.innerText = answers.option1
-    option2.innerText = answers.option2
-    shareTwitter.setAttribute("href", getUrlShareTwitter(openIAResult.text));
+    const currentQuestion = openIAResult.trim();
+    setup(currentQuestion)
   hideSpinner()
 }
 
@@ -46,4 +33,15 @@ function splitAnswer(quesiton) {
   let option1 = options.split( " or ")[0].split(action)[1]
   let option2 = options.split( " or ")[1].slice(0, -1)
   return {option1, option2}
+}
+
+function setup(currentQuestion) {
+  question.innerText = currentQuestion
+  const answers = splitAnswer(currentQuestion)
+  option1.innerText = answers.option1
+  option2.innerText = answers.option2
+  option1Button.setAttribute("value", answers.option1)
+  option2Button.setAttribute("value", answers.option2)
+  const answerSelected = document.querySelector('input[name=option]:checked').value;
+  shareTwitter.setAttribute("href", getUrlShareTwitter(currentQuestion, answerSelected));
 }
--- js/selectors.js
+++ js/selectors.js
@@ -1,6 +1,4 @@
 export const generateButton = document.querySelector(".generate-button");
-export const openIAOutput = document.querySelector(".openIA-question");
-export const cohereOutput = document.querySelector(".cohere-question");
 export const shareTwitter = document.querySelector(".share-twitter");
 export const openIATime = document.querySelector(".openIATime");
 export const cohereTime = document.querySelector(".cohereTime");
