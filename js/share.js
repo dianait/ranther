caf51c18060471f92c0c4ff6c03c4e8bbda50376
Diana Hernández <dianahdezsoler@gmail.com>
2023-07-09 19:37:52 +0200

    Update share twitter text

--- js/share.js
+++ js/share.js
@@ -1,5 +1,5 @@
 export function getUrlShareTwitter(question, answer) {
     const BASE_URL = "https://twitter.com/intent/tweet";
-    const content = `?text= My answer was ${answer} to ${question} What's yours? 💪 @dianait_ Get another one at &url=https://ranther.vercel.app`;
+    const content = `?text= My answer was ${answer} to ${question}. What's yours? 💪 @dianait_ Get another one at &url=https://ranther.vercel.app`;
     return BASE_URL + content;
   } 
